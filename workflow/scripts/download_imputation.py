8a7aeca97fcbf7baff184e903ac93928ac369f6e
Brian Fulton-Howard <fultonh1@gmail.com>
2025-02-06 13:44:29 -0500

    Make imputation download print log lines instead of overwriting output

--- workflow/scripts/download_imputation.py
+++ workflow/scripts/download_imputation.py
@@ -1,34 +1,77 @@
-#!/usr/bin/env python
-
 import json
 import requests
 import datetime
 import time
+import logging
 from urllib.parse import urlparse
 import urllib
 import sys
 import os
+import re
 
-# jsonfile = 'intermediate/pre-impute_prep/imputation/EUR_imputation.json'
-# imputation = {'url': 'https://imputationserver.sph.umich.edu/api/v2',
-#               'token': 'eyJjdHkiOiJ0ZXh0XC9wbGFpbiIsImFsZyI6IkhTMjU2In0.eyJtYWlsIjoiZnVsdG9uaDFAZ21haWwuY29tIiwiZXhwaXJlIjoxNjQxMDUyNDc0NjMxLCJuYW1lIjoiQnJpYW4gRSBGdWx0b24tSG93YXJkIiwiYXBpIjp0cnVlLCJ1c2VybmFtZSI6ImJlZmgifQ.IhGvMBoHG12qCpjuAndPldZwGnRwBklB35xpQl2UjAM',
-#               'id': 'job-20211207-143135-505'}
-# imputation['id'] = 'job-20171123-122635-549'
+if 'snakemake' not in globals():
+    import yaml
+    with open("config/config.yaml", 'r') as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+    class snakemake_class_testing:
+        input = []
+        params = {}
+    snakemake = snakemake_class_testing()
+    snakemake.params['token'] = cfg["impute"]["imputation"]["default"]["token"]
+    snakemake.params['outpath'] = "temp/sandbox"
+    os.makedirs(snakemake.params['outpath'], exist_ok=True)
+    snakemake.input = ["intermediate/imputation/imputation/nacc-gsa_EAS_imputation.json"]
 
-def progress(count, block_size, total_size):
+
+# Configure logging
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+
+
+def format_speed(speed):
+    """Makes download speed in human-readable."""
+    units = ["B/s", "KB/s", "MB/s", "GB/s"]
+    precision = 0
+    while speed >= 1024 and len(units) > 1:
+        speed /= 1024
+        units.pop(0)
+        precision += 1
+    return f"{speed:.{min(precision, 3)}f} {units[0]}"
+    
+
+def progress(file_name, count, block_size, total_size):
+    """Prints download progress every 15 seconds with file name."""
+    global last_progress_time
     global start_time
-    if count == 0:
-        start_time = time.time()
-        return
-    duration = time.time() - start_time
-    progress_size = int(count * block_size)
-    speed = int(progress_size / (1024 * duration))
-    percent = min(int(count * block_size * 100 / total_size), 100)
-    sys.stdout.write('\r  {}%, {} MB, {} KB/s, {} seconds passed'.format(
-        percent, progress_size // (1024 * 1024), int(speed), int(duration)))
+    global reported
 
+    now = time.time()
+
+    if count == 0:
+        last_progress_time = now
+        start_time = now
+        reported = False
+        return
+    size = int(count * block_size)
+    progress_size = f"{size // (1024 * 1024)} MB"
+    percent = min(int(count * block_size * 100 / total_size), 100)
+    speed = format_speed(size / (now - start_time))
+
+    initial_report = False
+    if now - start_time >= 2 and not reported:
+        initial_report = True
+        reported = True
+
+    if now - last_progress_time >= 15 or initial_report:  # Update every 15 seconds
+        last_progress_time = now
+        logging.info(f"Downloading {file_name}: {percent}% | {progress_size} | {speed}")
 
 def jobinfo(settings, token=None):
+    """Fetch job information from the API."""
     settings['token'] = token if token is not None else settings['token']
     r_check = requests.get(
         '{u}/jobs/{j}'.format(u=settings['url'], j=settings['id']),
@@ -41,24 +84,21 @@ def jobinfo(settings, token=None):
             else:
                 return jobinfo(settings, token=token_config)
         else:
-            raise Exception('GET /jobs/{}/status {}'.format(
-                settings['id'], r_check.status_code))
+            raise Exception(f"GET /jobs/{settings['id']}/status {r_check.status_code}")
     jinfo = r_check.json()
     jinfo['settings'] = settings
     return jinfo
 
-
 def fmt_delta(start_time):
+    """Formats elapsed time as HH:MM:SS."""
     delt = datetime.datetime.now() - start_time
     hours = delt.seconds // 3600
     mins = (delt.seconds % 3600) // 60
     sec = (delt.seconds % 3600) % 60
-    ts = '{h:02d}:{m:02d}:{s:02d}'.format(h=hours, m=mins, s=sec)
-    if delt.days > 0:
-        return '{D} Days {t}'.format(D=delt.days, t=ts)
-    return ts
+    ts = f"{hours:02d}:{mins:02d}:{sec:02d}"
+    return f"{delt.days} Days {ts}" if delt.days > 0 else ts
 
-
+# Read job details
 jsonfile = snakemake.input[0]
 with open(jsonfile, 'r') as j:
     imputation = json.load(j)
@@ -66,28 +106,31 @@ with open(jsonfile, 'r') as j:
 jinfo = jobinfo(imputation)
 
 if jinfo['state'] < 4:
-    print('Waiting for {} to complete.\n\n'.format(imputation['id']))
+    logging.info(f"Waiting for {imputation['id']} to complete.")
 
-submission = datetime.datetime.fromtimestamp(jinfo['submittedOn']/1000)
+submission = datetime.datetime.fromtimestamp(jinfo['submittedOn'] / 1000)
 
+friendly_jobname = re.sub(r'_submitted20\d\d-\d\d-\d\d\.\d+$', '', jinfo["name"])
+
+
+# Monitor job status
 while jinfo['state'] < 4:
-    print("\033[A                                      \033[A")
     if jinfo['state'] == 1:
         print('{} waiting for {}. (Queue position {})'.format(
             imputation['id'], fmt_delta(submission), jinfo['positionInQueue']))
         time.sleep(60) # wait for 1 minute
     elif jinfo['state'] == 2:
-        start_time = datetime.datetime.fromtimestamp(jinfo['startTime']/1000)
-        print('{} running for {}.'.format(
-            imputation['id'], fmt_delta(start_time)))
-        time.sleep(20) # wait for 20 sec
-    elif jinfo['state'] == 2:
-        print('{} exporting.'.format(imputation['id']))
-        time.sleep(20) # wait for 20 sec
+        start_time = datetime.datetime.fromtimestamp(jinfo['startTime'] / 1000)
+        logging.info(f"{imputation['id']} running for {fmt_delta(start_time)}.")
+        time.sleep(20)
+    elif jinfo['state'] == 3:
+        logging.info(f"{imputation['id']} exporting.")
+        time.sleep(20)
     else:
-        raise Exception('{} died.'.format(imputation['id']))
+        raise Exception(f"{imputation['id']} died.")
     jinfo = jobinfo(jinfo['settings'])
 
+# Prepare downloads
 url_dl = 'https://{fqdn}/share/results/{{hash}}/{{name}}'.format(
     fqdn=urlparse(imputation['url']).netloc)
 
@@ -99,36 +142,31 @@ outputs = [{**x, 'download': [{'url_dl': url_dl.format(**y),
 outputs_dict = {x['description']: x['download']
                 for x in outputs if x['download']}
 
+# download files
 if jinfo['state'] not in [7, 10]:
     for desc, dl in outputs_dict.items():
-        print('Downloading ' + desc)
+        logging.info(f"Downloading {desc} for {friendly_jobname}")
         for file in dl:
-            destfile = os.path.join(snakemake.params['outpath'],
-                                    file['filename'])
-            print('\nFile: {} ...'.format(file['filename']))
+            destfile = os.path.join(snakemake.params['outpath'], file['filename'])
+            logging.info(f"Starting download: {file['filename']} for {friendly_jobname}")
             urllib.request.urlretrieve(
-                file['url_dl'], filename=destfile, reporthook=progress)
-        print('\n')
+                file['url_dl'], filename=destfile,
+                reporthook=lambda c, bs, ts: progress(f"{friendly_jobname} {file['filename']}", c, bs, ts))
+        logging.info(f"Finished downloading {desc} for {friendly_jobname}")
 
+# Save job info
 with open(os.path.join(snakemake.params['outpath'], 'job.json'), 'w') as f:
     json.dump(jinfo, f)
 
+# Handle job completion status
 if jinfo['state'] in [4, 8]:
-    print('{} was successful.'.format(imputation['id']))
+    logging.info(f"{imputation['id']} was successful.")
 elif jinfo['state'] in [5, 6, 7, 9, 10]:
     state = {6: 'cancelled', 7: 'retired', 10: 'deleted',
              5: 'failed', 9: 'failed'}[jinfo['state']]
     state += '. Check logs' if jinfo['state'] in [5, 9] else '. Please rerun'
-    raise Exception('{} was {}.'.format(imputation['id'], state))
+    logging.error(f"{imputation['id']} was {state}.")
+    sys.exit(1)
 else:
-    raise Exception('{} status not recognized. Status = {}'.format(
-        imputation['id'], jinfo['state']))
-
-def human_readable_size(size, decimal_places=4):
-    for unit in ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB']:
-        if size < 1024.0 or unit == 'PiB':
-            break
-        size /= 1024.0
-    return f"{size:.{decimal_places}f} {unit}"
-
-#human_readable_size(98558729)
+    logging.error(f"{imputation['id']} status not recognized. Status = {jinfo['state']}")
+    sys.exit(1)
